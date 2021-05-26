c3f9cacc89a53f12e2222e01b58a0404585513d3
James Maila <james.maila@forgeglobal.com>
2021-05-26 14:34:12 -0600

    add azure func

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,24 @@ import SmallCard from '../components/SmallCard';
 import { projectIcons } from '../components/Icons';
 
 import { projects } from '../utils/projectsData';
+import React, { useState, useEffect } from 'react';
 
-const Home = () => (
+const Home = () => {
+
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/Test`)).json();
+      setData(text);
+    })();
+  });
+
+  return (
   <div className="home">
     <h1>Hey Tyson!!! This is a nextjs azure static web app!!!</h1>
-  </div>
-);
+    <span>What does the API have to say? <b>{data}</b></span>
+  </div>)
+}
 
 export default Home;
